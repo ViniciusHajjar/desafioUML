6172e790edf6ab8dd71b6b42827d5f8b310795e7
ViniciusHajjar <viniciushajjar19@gmail.com>
2023-10-21 23:13:09 -0300

    Initial commit

---
+++ src/Iphone.java
@@ -0,0 +1,50 @@
+public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+
+    @Override
+    public void ligar() {
+        
+    }
+
+    @Override
+    public void atender() {
+        
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        
+    }
+
+    @Override
+    public void tocar() {
+        
+    }
+
+    @Override
+    public void pausar() {
+        
+    }
+
+    @Override
+    public void selecionarMusica() {
+        
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        
+    }
+
+    @Override
+    public void atualizarPagina() {
+        
+    }
+
+    @Override
+    public void exibirPagina() {
+        
+    }
+
+    
+    
+}
